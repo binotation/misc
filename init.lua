836c0fdb199e1d05f3b7e07fe8220600d516151d
binotation <68807919+binotation@users.noreply.github.com>
2022-08-26 12:23:43 +1000

    Added init.lua

---
+++ init.lua
@@ -0,0 +1,20 @@
+local cmd = vim.cmd
+local fn = vim.fn
+local g = vim.g
+local opt = vim.opt
+
+require "paq" {
+    "savq/paq-nvim";                  -- Let Paq manage itself
+
+    "neovim/nvim-lspconfig";          -- Mind the semi-colons
+    "hrsh7th/nvim-cmp";
+    "sainnhe/everforest";
+
+    {"lervag/vimtex", opt=true};      -- Use braces when passing options
+}
+
+cmd "colorscheme everforest"
+opt.number = true
+opt.showmatch = true
+opt.tabstop = 4                       -- width of tab character in spaces
+opt.shiftwidth = 4                    -- size of an indent in spaces
