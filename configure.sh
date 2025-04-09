0becdae7f73eead92ef57acb0d837b60012e2f5f
JN <68807919+binotation@users.noreply.github.com>
2025-04-09 10:57:22 +1000

    Fix nvim uri

--- configure.sh
+++ configure.sh
@@ -23,7 +23,7 @@ mkdir -p "$HOME"/.config/nvim
 cp misc/init.lua "$HOME"/.config/nvim
 mkdir -p "$HOME"/.local/bin
 echo 'export PATH="$PATH":"$HOME"/.local/bin' >> "$HOME"/.bashrc
-(cd "$HOME"/Downloads && curl -OL https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux64.tar.gz && tar xzf nvim-linux64.tar.gz && cp -r nvim-linux64/* "$HOME"/.local)
+(cd "$HOME"/Downloads && curl -OL https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz && tar xzf nvim-linux-x86_64.tar.gz && cp -r nvim-linux-x86_64/* "$HOME"/.local)
 git clone --depth=1 https://github.com/savq/paq-nvim.git \
     "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
 echo 'alias vi=nvim' >> "$HOME"/.bashrc
